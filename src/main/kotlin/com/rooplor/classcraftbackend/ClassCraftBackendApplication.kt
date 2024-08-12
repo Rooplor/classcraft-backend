c26b3616912ab1f8f2233f1301d16ab5d58fef29
Natdanai Nitipan <c3bosskung@gmail.com>
2024-08-12 19:57:22 +0700

    init project kotlin

---
+++ src/main/kotlin/com/rooplor/classcraftbackend/ClassCraftBackendApplication.kt
@@ -0,0 +1,19 @@
+package com.rooplor.classcraftbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class ClassCraftBackendApplication {
+	@GetMapping("/hello")
+	fun hello(): String {
+		return "hello"
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<ClassCraftBackendApplication>(*args)
+}
