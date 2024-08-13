a07da95374b042584468f023e523de0342ec8172
Natdanai Nitipan <c3bosskung@gmail.com>
2024-08-13 13:13:14 +0700

    restructure code

--- src/main/kotlin/com/rooplor/classcraftbackend/Controller/HelloController.kt
+++ src/main/kotlin/com/rooplor/classcraftbackend/Controller/HelloController.kt
@@ -1,8 +1,7 @@
-package com.rooplor.classcraftbackend
+package com.rooplor.classcraftbackend.Controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/api/hello")
