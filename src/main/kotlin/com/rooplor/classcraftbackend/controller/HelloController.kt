584b789794f50e92696c4256311fbc388aa838d4
Natdanai Nitipan <c3bosskung@gmail.com>
2024-08-14 01:21:32 +0700

    fix annotation

--- src/main/kotlin/com/rooplor/classcraftbackend/controller/HelloController.kt
+++ src/main/kotlin/com/rooplor/classcraftbackend/controller/HelloController.kt
@@ -2,9 +2,11 @@ package com.rooplor.classcraftbackend.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/hello")
+@RestController
+@RequestMapping("/api/hello")
 class HelloController {
     @GetMapping("/{name}")
     fun hello(
