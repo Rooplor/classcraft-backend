2ff07bfbb00e28ffd53313633418c195755677fa
Natdanai Nitipan <62214133+c3bosskung@users.noreply.github.com>
2024-08-13 23:40:41 +0700

    Init ktlint (#5)

--- src/main/kotlin/com/rooplor/classcraftbackend/controller/HelloController.kt
+++ src/main/kotlin/com/rooplor/classcraftbackend/controller/HelloController.kt
@@ -1,4 +1,4 @@
-package com.rooplor.classcraftbackend.Controller
+package com.rooplor.classcraftbackend.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController("/api/hello")
 class HelloController {
     @GetMapping("/{name}")
-    fun hello(@PathVariable name: String): String {
-        return "Hello, $name!"
-    }
+    fun hello(
+        @PathVariable name: String,
+    ): String = "Hello, $name!"
 }
