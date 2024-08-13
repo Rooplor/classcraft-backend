48452b8314569071aa03b1c7613ef9363c1f4807
Natdanai Nitipan <62214133+c3bosskung@users.noreply.github.com>
2024-08-13 22:12:16 +0700

    init-swagger (#3)

--- src/main/kotlin/com/rooplor/classcraftbackend/ClassCraftBackendApplication.kt
+++ src/main/kotlin/com/rooplor/classcraftbackend/ClassCraftBackendApplication.kt
@@ -2,18 +2,10 @@ package com.rooplor.classcraftbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-@RestController
-class ClassCraftBackendApplication {
-	@GetMapping("/hello")
-	fun hello(): String {
-		return "hello"
-	}
-}
+class ClassCraftBackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<ClassCraftBackendApplication>(*args)
+    runApplication<ClassCraftBackendApplication>(*args)
 }
