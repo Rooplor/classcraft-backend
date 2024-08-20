5732dbc5862d9c1e3040499dc028e06dcb337163
Natdanai Nitipan <c3bosskung@gmail.com>
2024-08-21 02:40:28 +0700

    fix unit-test modelmapper missing

--- src/test/kotlin/com/rooplor/classcraftbackend/controllers/ClassControllerTest.kt
+++ src/test/kotlin/com/rooplor/classcraftbackend/controllers/ClassControllerTest.kt
@@ -1,16 +1,19 @@
 package com.rooplor.classcraftbackend.controllers
 
+import com.rooplor.classcraftbackend.dtos.InitClassDTO
 import com.rooplor.classcraftbackend.entities.Class
 import com.rooplor.classcraftbackend.enums.Format
 import com.rooplor.classcraftbackend.enums.Type
 import com.rooplor.classcraftbackend.services.ClassService
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(ClassController::class)
@@ -20,6 +23,9 @@ class ClassControllerTest {
 
     @MockBean
     private lateinit var classService: ClassService
+
+    @MockBean
+    private lateinit var modelMapper: ModelMapper
 
     @Test
     fun `should return all classes`() {
@@ -88,11 +94,23 @@ class ClassControllerTest {
                 capacity = 30,
                 date = listOf(),
             )
+        val initClassDTO =
+            InitClassDTO(
+                title = "Test Class",
+                details = "Details",
+                target = "Target",
+                prerequisite = "None",
+                type = Type.LECTURE,
+                format = Format.ONSITE,
+                capacity = 30,
+                date = listOf(),
+            )
+        Mockito.`when`(modelMapper.map(initClassDTO, Class::class.java)).thenReturn(classObj)
         Mockito.`when`(classService.insertClass(classObj)).thenReturn(classObj)
 
         mockMvc
             .perform(
-                get("/api/class")
+                post("/api/class")
                     .contentType("application/json")
                     .content(
                         """
