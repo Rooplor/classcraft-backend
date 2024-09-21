2b1a90dcf9376f5359321c97e3430d4eb547d490
Nine0512 <nineza08088@gmail.com>
2024-09-21 18:00:58 +0700

    add ErrorMessages

--- src/main/kotlin/com/rooplor/classcraftbackend/configuration/FirebaseConfig.kt
+++ src/main/kotlin/com/rooplor/classcraftbackend/configuration/FirebaseConfig.kt
@@ -3,11 +3,15 @@ package com.rooplor.classcraftbackend.configuration
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.rooplor.classcraftbackend.messages.ErrorMessages
+import com.rooplor.classcraftbackend.services.logger.LoggerService
 import org.springframework.context.annotation.Configuration
 import java.io.FileInputStream
 
 @Configuration
-class FirebaseConfig {
+class FirebaseConfig(
+    private val loggerService: LoggerService,
+) {
     init {
         try {
             val serviceAccount = FileInputStream("src/main/resources/serviceAccountKey.json")
@@ -20,7 +24,7 @@ class FirebaseConfig {
 
             FirebaseApp.initializeApp(option)
         } catch (e: Exception) {
-            e.printStackTrace()
+            loggerService.error(ErrorMessages.FIREBASE_CONFIG_ERROR + ": $e")
         }
     }
 }
---
+++ src/main/kotlin/com/rooplor/classcraftbackend/messages/ErrorMessages.kt
@@ -0,0 +1,5 @@
+package com.rooplor.classcraftbackend.messages
+
+object ErrorMessages {
+    const val FIREBASE_CONFIG_ERROR = "Firebase configuration error"
+}
