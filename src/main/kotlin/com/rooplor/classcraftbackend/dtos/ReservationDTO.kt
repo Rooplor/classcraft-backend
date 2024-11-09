99f360ac730198f39d40934ae9d003d686003c76
Natdanai Nitipan <c3bosskung@gmail.com>
2024-11-09 08:23:52 +0700

    add JsonProperty to ReservationDTO

--- src/main/kotlin/com/rooplor/classcraftbackend/dtos/ReservationDTO.kt
+++ src/main/kotlin/com/rooplor/classcraftbackend/dtos/ReservationDTO.kt
@@ -1,5 +1,8 @@
 package com.rooplor.classcraftbackend.dtos
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ReservationDTO(
+    @JsonProperty("venueId")
     var venueId: List<String>,
 )
