ed5431e46b26a67596a2fd2b2abc730966eb9bea
Natdanai Nitipan <c3bosskung@gmail.com>
2024-11-09 10:31:17 +0700

    fix ktlint failed

--- src/test/kotlin/com/rooplor/classcraftbackend/controllers/ClassroomControllerTest.kt
+++ src/test/kotlin/com/rooplor/classcraftbackend/controllers/ClassroomControllerTest.kt
@@ -11,7 +11,9 @@ import com.rooplor.classcraftbackend.enums.VenueStatus
 import com.rooplor.classcraftbackend.services.ClassService
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
-import org.mockito.Mockito.*
+import org.mockito.Mockito.doNothing
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
