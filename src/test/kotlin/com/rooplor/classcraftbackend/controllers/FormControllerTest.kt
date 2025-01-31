8b214aa9e8c47f103ccf74ceb915b122f25780b0
Nine0512 <nineza0808@gmail.com>
2025-01-31 23:02:25 +0700

    Fix lint

--- src/test/kotlin/com/rooplor/classcraftbackend/controllers/FormControllerTest.kt
+++ src/test/kotlin/com/rooplor/classcraftbackend/controllers/FormControllerTest.kt
@@ -21,7 +21,11 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
