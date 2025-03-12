a4eb13fa738c83f8af1b9e163f2b0fa6a9d24e30
Natdanai Nitipan <c3bosskung@gmail.com>
2025-03-13 06:05:17 +0700

    add css

--- src/main/kotlin/com/rooplor/classcraftbackend/messages/MailMessage.kt
+++ src/main/kotlin/com/rooplor/classcraftbackend/messages/MailMessage.kt
@@ -3,6 +3,10 @@ package com.rooplor.classcraftbackend.messages
 object MailMessage {
     const val REGISTRATION_SUBJECT = "Confirmation of your registration"
     const val TOPIC_REGISTRATION = "You’ve registered for "
-    const val REGISTRATION_SUCCESS = "You’ve successfully registered for the class. We look forward to seeing you there!"
-    const val REGISTRATION_PENDING = "Your registration is pending approval. We will notify you once it’s approved."
+    const val REGISTRATION_SUCCESS =
+        "You’ve <span style=\"font-weight: bold; color: green;\">successfully</span> " +
+            "registered for the class. We look forward to seeing you there!"
+    const val REGISTRATION_PENDING =
+        "Your registration is <span style=\"font-weight: bold; color: yellow;\">pending</span> " +
+            "approval. We will notify you once it’s approved."
 }
